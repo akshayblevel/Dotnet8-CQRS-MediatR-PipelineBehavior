0276f66556aef56199126099669b9831994f4765
Akshay Patel <akshayblevel@gmail.com>
2024-04-06 21:34:43 +0530

    Initial Commit

---
+++ Dotnet8-CQRS-MediatR-PipelineBehavior/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Dotnet8_CQRS_MediatR_PipelineBehavior.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found!")
+        {
+
+        }
+    }
+}
